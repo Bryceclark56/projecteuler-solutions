a26851579084396c00e05590484a9a52b00c6007
Bryce Clark <bryce@lowquality.dev>
2021-04-17 01:22:19 -0500

    P1: Basic solution code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+/* Project Euler
+ *  Problem 1 
+ *  
+ * Find the sum of all the multiples of 3 or 5 below N,
+ *  where N is a natural number.
+ */
+
 fn main() {
-    println!("Hello, world!");
+    const N: isize = 1000; // Constant N for now, default + cli argument later.
+
+    println!("Project Euler - Problem 1");
+    println!("=========================");
+
+    println!("N = {}", N);
+    println!("\nRunning calculation...\n");
+
+
+    let mut sum = 0;
+
+    for i in 1..N {
+        if i % 3 == 0 {
+            sum += i;
+        }
+        else if i % 5 == 0 {
+            sum += i;
+        }
+    }
+
+    println!("Sum: {}", sum);
 }
