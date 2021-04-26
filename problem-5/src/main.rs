eb319cab7bdbef58ebf30eb529eaf68f7a420fa6
Bryce Clark <bryce@lowquality.dev>
2021-04-25 20:22:19 -0500

    P5: Add template

--- problem-5/src/main.rs
+++ problem-5/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+/* Project Euler
+ *  Problem 5
+ *  
+ * Find the smallest positive number 
+ *  that is evenly divisible by all of 
+ *  the numbers from 1 to 20.
+ */
+
+ fn main() {
+    println!("Project Euler - Problem 5");
+    println!("=========================\n");
 }
